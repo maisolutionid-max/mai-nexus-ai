d11bf8d79a98a0956c9eb5ed84783591fb709000
maisolutionid-max <maisolution.id@gmail.com>
2026-07-23 13:01:24 +0700

    Connect customer API to SQLite

--- backend/app/api/customer_api.py
+++ backend/app/api/customer_api.py
@@ -2,17 +2,41 @@ from fastapi import APIRouter
 from app.models.customer import Customer
 
 router = APIRouter()
-
-customers = []
+from app.database.database import get_connection
 
 @router.get("/customers")
 def get_customers():
-    return customers
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM customers")
+
+    customers = cursor.fetchall()
+
+    conn.close()
+
+    return [dict(row) for row in customers]
 
 @router.post("/customers")
 def create_customer(customer: Customer):
-    customers.append(customer)
+
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "INSERT INTO customers(name,phone,address) VALUES (?,?,?)",
+        (
+            customer.name,
+            customer.phone,
+            customer.address
+        )
+    )
+
+    conn.commit()
+
+    conn.close()
+
     return {
-        "message": "Customer berhasil ditambahkan",
-        "data": customer
+        "message": "Customer berhasil ditambahkan"
     }
+    
