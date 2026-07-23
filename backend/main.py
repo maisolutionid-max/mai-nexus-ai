b52365ba1f438d682ac2fe86e82bd8925758b6e4
maisolutionid-max <maisolution.id@gmail.com>
2026-07-23 13:34:52 +0700

    Fix main.py initialization

--- backend/main.py
+++ backend/main.py
@@ -4,16 +4,19 @@ from app.api.order_api import router as order_router
 from app.api.dashboard_api import router as dashboard_router
 from app.auth.login_api import router as login_router
 from app.database.init_db import init_database
-init_database()
+
 app = FastAPI(
     title="MAI Nexus AI",
     version="1.0.0"
 )
 
+init_database()
+
 app.include_router(customer_router)
 app.include_router(order_router)
 app.include_router(dashboard_router)
 app.include_router(login_router)
+
 @app.get("/")
 def home():
     return {
