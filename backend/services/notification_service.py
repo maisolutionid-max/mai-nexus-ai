3161189f39edc79b45506d20168de397a85b3b67
maisolutionid-max <maisolution.id@gmail.com>
2026-07-23 20:45:15 +0700

    Create notification_service.py

---
+++ backend/services/notification_service.py
@@ -0,0 +1,14 @@
+"""
+Notification Service
+"""
+
+class NotificationService:
+
+    def send_whatsapp(self, phone, message):
+        pass
+
+    def send_email(self, email, message):
+        pass
+
+    def send_push_notification(self, user_id, message):
+        pass
