6d3ffde96a9554371508a40d6c0f5d697f006f53
maisolutionid-max <maisolution.id@gmail.com>
2026-07-23 12:26:09 +0700

    Update order_api.py

--- backend/app/api/order_api.py
+++ backend/app/api/order_api.py
@@ -1,16 +1,18 @@
 from fastapi import APIRouter
+from app.models.order import Order
 
 router = APIRouter()
 
+orders = []
+
 @router.get("/orders")
 def get_orders():
-    return [
-        {
-            "id": 1,
-            "customer": "Budi Santoso",
-            "service": "Cuci Kering",
-            "weight": 5,
-            "total": 35000,
-            "status": "Diterima"
-        }
-    ]
+    return orders
+
+@router.post("/orders")
+def create_order(order: Order):
+    orders.append(order)
+    return {
+        "message": "Order berhasil ditambahkan",
+        "data": order
+    }
