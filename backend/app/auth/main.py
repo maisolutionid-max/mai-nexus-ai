bf17a7d6b0f0eacbb0de3a38fe5e0b9bb11b011e
maisolutionid-max <maisolution.id@gmail.com>
2026-07-23 14:40:47 +0700

    Create main.py

---
+++ backend/app/auth/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="MAI Nexus AI",
+    version="1.0.0"
+)
+
+@app.get("/")
+def home():
+    return {
+        "message": "Welcome to MAI Nexus AI Backend"
+    }
