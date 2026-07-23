421b6ddc84b1c3a39b27698886cf1329b7ab488e
maisolutionid-max <maisolution.id@gmail.com>
2026-07-23 12:46:30 +0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.api.customer_api import router as customer_router
 from app.api.order_api import router as order_router
+from app.api.dashboard_api import router as dashboard_router
 app = FastAPI(
     title="MAI Nexus AI",
     version="1.0.0"
@@ -8,6 +9,7 @@ app = FastAPI(
 
 app.include_router(customer_router)
 app.include_router(order_router)
+app.include_router(dashboard_router)
 @app.get("/")
 def home():
     return {
