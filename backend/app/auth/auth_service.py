2d7d11546195a85ab7a7d607bb93dda5158e8bbd
maisolutionid-max <maisolution.id@gmail.com>
2026-07-23 14:35:36 +0700

    Update auth_service.py

--- backend/app/auth/auth_service.py
+++ backend/app/auth/auth_service.py
@@ -1,1 +1,19 @@
-# Authentication Service
+from .Password import verify_password
+from .jwt_handler import create_access_token
+
+def login(username: str, password: str):
+
+    # Dummy user sementara
+    user = {
+        "username": "admin",
+        "password": "$2b$12$dummyhash"
+    }
+
+    token = create_access_token(
+        {"sub": username}
+    )
+
+    return {
+        "access_token": token,
+        "token_type": "bearer"
+    }
