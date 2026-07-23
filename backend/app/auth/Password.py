21fceee0c303414ce6269cce2e4d8597349cf32c
maisolutionid-max <maisolution.id@gmail.com>
2026-07-23 14:31:50 +0700

    Update Password.py

--- backend/app/auth/Password.py
+++ backend/app/auth/Password.py
@@ -1,12 +1,9 @@
 from passlib.context import CryptContext
 
-pwd_context = CryptContext(
-    schemes=["bcrypt"],
-    deprecated="auto"
-)
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def hash_password(password: str):
+def hash_password(password: str) -> str:
     return pwd_context.hash(password)
 
-def verify_password(password: str, hashed_password: str):
+def verify_password(password: str, hashed_password: str) -> bool:
     return pwd_context.verify(password, hashed_password)
