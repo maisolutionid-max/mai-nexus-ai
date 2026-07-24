844c68c189018607c95ba10b3e59a42c29b11d64
maisolutionid-max <maisolution.id@gmail.com>
2026-07-24 13:20:32 +0700

    Update payment_router.py

--- backend/routers /payment_router.py
+++ backend/routers /payment_router.py
@@ -1,1 +1,21 @@
-payment_router
+from fastapi import APIRouter
+
+router = APIRouter(
+    prefix="/payments",
+    tags=["Payments"]
+)
+
+@router.get("/")
+def get_payments():
+    return {
+        "status": "success",
+        "message": "Daftar pembayaran",
+        "data": []
+    }
+
+@router.post("/")
+def create_payment():
+    return {
+        "status": "success",
+        "message": "Pembayaran berhasil dibuat"
+    }
