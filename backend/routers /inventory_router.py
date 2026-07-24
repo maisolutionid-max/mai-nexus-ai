85cd25c3ae5df57cf868fd0bf306249ddd156fd1
maisolutionid-max <maisolution.id@gmail.com>
2026-07-24 13:19:22 +0700

    Update inventory_router.py

--- backend/routers /inventory_router.py
+++ backend/routers /inventory_router.py
@@ -1,1 +1,21 @@
-inventory_router
+from fastapi import APIRouter
+
+router = APIRouter(
+    prefix="/inventory",
+    tags=["Inventory"]
+)
+
+@router.get("/")
+def get_inventory():
+    return {
+        "status": "success",
+        "message": "Daftar stok",
+        "data": []
+    }
+
+@router.post("/")
+def add_inventory():
+    return {
+        "status": "success",
+        "message": "Stok berhasil ditambahkan"
+    }
