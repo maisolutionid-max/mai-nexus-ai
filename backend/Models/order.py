f5603eaa3df39699591e7b562383932b045ed474
maisolutionid-max <maisolution.id@gmail.com>
2026-07-24 17:18:17 +0700

    Update order.py

--- backend/Models/order.py
+++ backend/Models/order.py
@@ -1,1 +1,12 @@
-satqbadr
+from pydantic import BaseModel
+
+class Order(BaseModel):
+    invoice_number: str
+    customer_id: int
+    service_id: int
+    weight: float
+    price: float
+    status: str
+    pickup_date: str
+    finish_date: str
+    notes: str
