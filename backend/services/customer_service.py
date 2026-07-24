1afa556938bac9211d61cacb2a1eabb280149b86
maisolutionid-max <maisolution.id@gmail.com>
2026-07-24 15:08:08 +0700

    Update customer_service.py

--- backend/services/customer_service.py
+++ backend/services/customer_service.py
@@ -1,1 +1,13 @@
-customer_service
+from database.Database import get_connection
+
+def get_all_customers():
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM customers")
+
+    data = cursor.fetchall()
+
+    conn.close()
+
+    return data
