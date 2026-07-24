e5fbefc2a5747077ba95c3a842fbfbe8735c7016
maisolutionid-max <maisolution.id@gmail.com>
2026-07-24 13:21:37 +0700

    Update ai_router.py

--- backend/routers /ai_router.py
+++ backend/routers /ai_router.py
@@ -1,1 +1,23 @@
-ai_router
+from fastapi import APIRouter
+
+router = APIRouter(
+    prefix="/ai",
+    tags=["AI"]
+)
+
+@router.get("/analysis")
+def ai_analysis():
+    return {
+        "status": "success",
+        "message": "AI Analysis",
+        "prediction": {},
+        "recommendation": "Data belum tersedia."
+    }
+
+@router.get("/dashboard")
+def ai_dashboard():
+    return {
+        "status": "success",
+        "message": "AI Dashboard",
+        "data": {}
+    }
