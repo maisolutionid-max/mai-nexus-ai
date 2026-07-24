4b98d6ea25f1fee45897098a9248e4837857d6e8
maisolutionid-max <maisolution.id@gmail.com>
2026-07-24 13:15:06 +0700

    Update user_router.py

--- backend/routers /user_router.py
+++ backend/routers /user_router.py
@@ -1,1 +1,21 @@
-user_router
+from fastapi import APIRouter
+
+router = APIRouter(
+    prefix="/users",
+    tags=["Users"]
+)
+
+@router.get("/")
+def get_users():
+    return {
+        "status": "success",
+        "message": "User API MAI Nexus AI",
+        "data": []
+    }
+
+@router.post("/")
+def create_user():
+    return {
+        "status": "success",
+        "message": "User berhasil dibuat"
+    }
