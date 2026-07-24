5733a76392494c6abf81625f9b5a9634ca0e9688
maisolutionid-max <maisolution.id@gmail.com>
2026-07-24 13:18:07 +0700

    Update service_router.py

--- backend/routers /service_router.py
+++ backend/routers /service_router.py
@@ -1,1 +1,21 @@
-service_router
+from fastapi import APIRouter
+
+router = APIRouter(
+    prefix="/services",
+    tags=["Services"]
+)
+
+@router.get("/")
+def get_services():
+    return {
+        "status": "success",
+        "message": "Daftar layanan laundry",
+        "data": []
+    }
+
+@router.post("/")
+def create_service():
+    return {
+        "status": "success",
+        "message": "Layanan berhasil ditambahkan"
+    }
