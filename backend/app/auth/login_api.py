8e449248ba07f71893f4ddc41984cd675559d495
maisolutionid-max <maisolution.id@gmail.com>
2026-07-26 04:46:49 +0700

    Create login_api

---
+++ backend/app/auth/login_api.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter
+
+router = APIRouter()
+
+users = [
+    {
+        "username": "owner",
+        "password": "123456",
+        "role": "Owner"
+    }
+]
+
+@router.post("/login")
+def login(data: dict):
+
+    for user in users:
+        if (
+            data["username"] == user["username"]
+            and
+            data["password"] == user["password"]
+        ):
+            return {
+                "message": "Login berhasil",
+                "role": user["role"]
+            }
+
+    return {
+        "message": "Username atau Password salah"
+    }
