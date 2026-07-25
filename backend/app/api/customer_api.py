c5f8de0fd1d71e095ba7489cfd910429d5bf1c11
maisolutionid-max <maisolution.id@gmail.com>
2026-07-25 19:12:06 +0700

    Update customer_api.py

--- backend/app/api/customer_api.py
+++ backend/app/api/customer_api.py
@@ -1,12 +1,18 @@
 from fastapi import APIRouter
+from app.models.customer import Customer
 
 router = APIRouter()
 
+customers = []
+
 @router.get("/customers")
 def get_customers():
-    return [
-        {
-            "name": "Budi",
-            "phone": "08123456789"
-        }
-    ]
+    return customers
+
+@router.post("/customers")
+def create_customer(customer: Customer):
+    customers.append(customer)
+    return {
+        "message": "Customer berhasil ditambahkan",
+        "data": customer
+    }
