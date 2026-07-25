2705845aa1bec5e90f25df445cbb898e4cb6117a
maisolutionid-max <maisolution.id@gmail.com>
2026-07-26 06:43:43 +0700

    Update auth_router.py

--- backend/app/auth/auth_router.py
+++ backend/app/auth/auth_router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from .auth_service import login
-
+from .user import User
+from .auth_service import register
 router = APIRouter(
     prefix="/auth",
     tags=["Authentication"]
@@ -9,3 +10,6 @@ router = APIRouter(
 @router.post("/login")
 def login_api(username: str, password: str):
     return login(username, password)
+@router.post("/register")
+def register_api(user: User):
+    return register(user)
