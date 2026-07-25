88329a94d1e6f91fa8348879a23b86f246af1eb7
maisolutionid-max <maisolution.id@gmail.com>
2026-07-26 06:56:56 +0700

    Update jwt_middleware.py

--- backend/app/middleware/jwt_middleware.py
+++ backend/app/middleware/jwt_middleware.py
@@ -1,1 +1,23 @@
-jet middleware
+from fastapi import HTTPException
+from jose import jwt
+
+SECRET_KEY = "mai_nexus_ai_secret"
+ALGORITHM = "HS256"
+
+def verify_token(token: str):
+
+    try:
+        payload = jwt.decode(
+            token,
+            SECRET_KEY,
+            algorithms=[ALGORITHM]
+        )
+
+        return payload
+
+    except Exception:
+
+        raise HTTPException(
+            status_code=401,
+            detail="Token tidak valid"
+        )
