f3446054054f40067fdccda2b5b0b76ea8557b45
maisolutionid-max <maisolution.id@gmail.com>
2026-07-26 06:29:36 +0700

    Update jwt_handler.py

--- backend/app/auth/jwt_handler.py
+++ backend/app/auth/jwt_handler.py
@@ -1,1 +1,23 @@
-jwt_handler.py
+from datetime import datetime, timedelta
+from jose import jwt
+
+SECRET_KEY = "mai_nexus_ai_secret"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+
+    expire = datetime.utcnow() + timedelta(
+        minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+    )
+
+    to_encode.update({"exp": expire})
+
+    encoded_jwt = jwt.encode(
+        to_encode,
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
+
+    return encoded_jwt
