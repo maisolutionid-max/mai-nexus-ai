ca675e1943e7d258baae8968693369f75a45d454
maisolutionid-max <maisolution.id@gmail.com>
2026-07-25 19:38:02 +0700

    Update order_api.py

--- backend/app/api/order_api.py
+++ backend/app/api/order_api.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from app.models.order import Order
-
+from app.services.pricing import calculate_price
 router = APIRouter()
 
 orders = []
@@ -11,7 +11,14 @@ def get_orders():
 
 @router.post("/orders")
 def create_order(order: Order):
+
+    order.total = calculate_price(
+        order.service,
+        order.weight
+    )
+
     orders.append(order)
+
     return {
         "message": "Order berhasil ditambahkan",
         "data": order
