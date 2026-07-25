8722e46b02bb6ccfda7b5202e3d83d880b113aaa
maisolutionid-max <maisolution.id@gmail.com>
2026-07-26 06:57:49 +0700

    Update role_middleware.py

--- backend/app/middleware/role_middleware.py
+++ backend/app/middleware/role_middleware.py
@@ -1,1 +1,12 @@
-role
+from fastapi import HTTPException
+
+def check_role(user_role: str, allowed_roles: list):
+
+    if user_role not in allowed_roles:
+
+        raise HTTPException(
+            status_code=403,
+            detail="Akses ditolak"
+        )
+
+    return True
