dcd42c7272dcd76466ec4bfa2208056d4aee054d
maisolutionid-max <maisolution.id@gmail.com>
2026-07-26 14:45:22 +0700

    Update payment.py

--- backend/app/models/payment.py
+++ backend/app/models/payment.py
@@ -1,1 +1,41 @@
+from sqlalchemy import Column, Integer, Float, String, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
 
+from database import Base
+
+
+class Payment(Base):
+    __tablename__ = "payments"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    order_id = Column(
+        Integer,
+        ForeignKey("orders.id"),
+        nullable=False
+    )
+
+    amount = Column(Float, nullable=False)
+
+    payment_method = Column(
+        String(30),
+        nullable=False
+    )
+
+    payment_status = Column(
+        String(30),
+        default="Pending"
+    )
+
+    payment_date = Column(
+        DateTime(timezone=True),
+        server_default=func.now()
+    )
+
+    created_at = Column(
+        DateTime(timezone=True),
+        server_default=func.now()
+    )
+
+    order = relationship("Order")
