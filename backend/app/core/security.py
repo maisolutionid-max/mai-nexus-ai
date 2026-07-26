5fb29e027353875a67c99a9544eb3509285e341d
maisolutionid-max <maisolution.id@gmail.com>
2026-07-26 12:44:53 +0700

    Update security.py

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,1 +1,28 @@
+from datetime import datetime, timedelta
+from jose import jwt
 
+from core.settings import settings
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    """
+    Membuat JWT Access Token.
+    """
+    to_encode = data.copy()
+
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(
+            minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
+        )
+
+    to_encode.update({"exp": expire})
+
+    encoded_jwt = jwt.encode(
+        to_encode,
+        settings.SECRET_KEY,
+        algorithm=settings.ALGORITHM
+    )
+
+    return encoded_jwt
