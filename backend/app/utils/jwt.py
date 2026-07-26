6be0a1aad0aa6cf58c21ae4a93786ad644a45b87
maisolutionid-max <maisolution.id@gmail.com>
2026-07-26 14:26:54 +0700

    Create jwt.py

---
+++ backend/app/utils/jwt.py
@@ -0,0 +1,29 @@
+from datetime import datetime, timedelta
+
+from jose import jwt
+
+from config import settings
+
+
+def create_access_token(
+    data: dict,
+    expires_delta: timedelta | None = None
+):
+    to_encode = data.copy()
+
+    expire = (
+        datetime.utcnow() + expires_delta
+        if expires_delta
+        else datetime.utcnow()
+        + timedelta(
+            minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
+        )
+    )
+
+    to_encode.update({"exp": expire})
+
+    return jwt.encode(
+        to_encode,
+        settings.SECRET_KEY,
+        algorithm=settings.ALGORITHM
+    )
