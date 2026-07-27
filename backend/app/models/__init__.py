5085a765d399fedf5c76a23b9f133dec89fdd10e
maisolutionid-max <maisolution.id@gmail.com>
2026-07-27 09:47:43 +0700

    Create __init__.py

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,17 @@
+from .user import User
+from .customer import Customer
+from .order import Order
+from .payment import Payment
+from .inventory import Inventory
+from .notification import Notification
+from .sensor import Sensor
+
+__all__ = [
+    "User",
+    "Customer",
+    "Order",
+    "Payment",
+    "Inventory",
+    "Notification",
+    "Sensor",
+]
