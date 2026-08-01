f70191281e4adaa55095ca9975736c37636b011b
maisolutionid-max <maisolution.id@gmail.com>
2026-08-01 14:18:01 +0700

    Update Order.jsx

--- frontend/src/pages/Order.jsx
+++ frontend/src/pages/Order.jsx
@@ -1,3 +1,11 @@
-export default function Order() {
-  return <h2>Order Page</h2>;
-}
+const columns=[
+"Order",
+"Customer",
+"Tanggal",
+"Status"
+];
+
+const data=[
+["ORD001","PT ABC","01 Aug","Processing"],
+["ORD002","PT XYZ","01 Aug","Completed"],
+];
