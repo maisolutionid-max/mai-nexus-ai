d09ea1a804bbf4c5d56173fbb4c5f9dfbda9ad33
maisolutionid-max <maisolution.id@gmail.com>
2026-08-01 14:13:27 +0700

    Update Customer.jsx

--- frontend/src/pages/Customer.jsx
+++ frontend/src/pages/Customer.jsx
@@ -1,3 +1,43 @@
-export default function Customer() {
-  return <h2>Customer Page</h2>;
+import MainLayout from "../layouts/MainLayout";
+import PageHeader from "../components/PageHeader";
+import DataTable from "../components/DataTable";
+
+export default function Customer(){
+
+const columns=[
+"ID",
+"Nama",
+"Email",
+"Telepon"
+];
+
+const data=[
+["1","PT ABC","abc@mail.com","08123456789"],
+["2","PT XYZ","xyz@mail.com","08129876543"],
+];
+
+return(
+
+<MainLayout>
+
+<PageHeader
+
+title="Customer"
+
+subtitle="Customer Management"
+
+/>
+
+<DataTable
+
+columns={columns}
+
+data={data}
+
+/>
+
+</MainLayout>
+
+);
+
 }
