b4ffa4b7ea624713e212c98b510e67d03217e1da
maisolutionid-max <maisolution.id@gmail.com>
2026-08-03 10:07:31 +0700

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ padding: "30px" }}>
+      <h1>Mai Nexus AI</h1>
+      <p>Frontend berhasil dijalankan.</p>
+    </div>
+  );
+}
